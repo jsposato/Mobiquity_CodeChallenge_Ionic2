44a562e6fa17fe1c7e34ac179a734c905a6ef534
Firoze Rakib <frakib@mobiquityinc.com>
2016-07-15 13:39:14 -0400

    moved constant variables to static Constants class

---
+++ MobCodeChallenge/app/constants.ts
@@ -0,0 +1,4 @@
+export class Constants {
+    static OPENWEATHERMAP_API_KEY = "9be27abdd75e963ba6b8dcf610966fb9";
+    static TEMPERATURE_UNIT = "F";
+}
--- MobCodeChallenge/app/pages/zip-code-details/zip-code-details.ts
+++ MobCodeChallenge/app/pages/zip-code-details/zip-code-details.ts
@@ -2,26 +2,25 @@ import {Component} from '@angular/core';
 import {Loading, NavController, NavParams} from 'ionic-angular';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {Constants} from '../../Constants';
 
 @Component({
   templateUrl: 'build/pages/zip-code-details/zip-code-details.html'
 })
 export class ZipCodeDetailsPage {
   zipcode: string;
-  openweathermap_api_key: string;
   zipcodeWeather: Object;
   currentUnit: string;
 
   constructor(private nav: NavController, navParams: NavParams, private http:Http) {
     this.zipcode = navParams.get('zipcode');
-    this.openweathermap_api_key = "9be27abdd75e963ba6b8dcf610966fb9";
     this.getWeatherForZipcode(this.zipcode);
-    this.currentUnit = "F";
+    this.currentUnit = Constants.TEMPERATURE_UNIT;
   }
   getWeatherForZipcode(zipcode) {
       let loading = Loading.create();
       this.nav.present(loading);
-      let url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=" + this.openweathermap_api_key;
+      let url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us&appid=" + Constants.OPENWEATHERMAP_API_KEY;
       this.http.get(url)
       .map(res => res.json())
       .subscribe(
@@ -43,7 +42,7 @@ export class ZipCodeDetailsPage {
       );
   }
   getTemperature(temp) {
-      if (this.currentUnit === "F") {
+      if (Constants.TEMPERATURE_UNIT === "F") {
           return ((parseFloat(temp) * 9/5) - 459.67).toFixed(2);
       } else {
           return (parseFloat(temp) - 273.15).toFixed(2);
